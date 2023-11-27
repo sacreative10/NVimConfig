dbbb4930fc3d7379764b92b61a97ae476daa7c41
Sohaib Alam <sacreative10@gmail.com>
2023-11-27 21:20:37 +0000

    Delete nvim directory

--- nvim/lua/custom/chadrc.lua
+++
@@ -1,7 +0,0 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {transparency = true, nvdash = {load_on_startup = true}}
- M.plugins = "custom.plugins"
- M.mappings = require("custom.mappings")
- 
- return M
